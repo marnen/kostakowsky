bf1408dc2853d46d030b35500dab985f98af0253
Marnen Laibow-Koser <marnen@marnen.org>
2018-04-04 16:25:51 -0400

    Build table of contents.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,15 +1,34 @@
 'use strict';
 import ABCJS from 'abcjs';
 import $ from 'jquery';
+
+const domId = ({id}) => `tune_${id}`;
+
+const metadata = tunes => tunes.map(tune => {
+  const abcLines = tune.abc.split('\n');
+  const keys = abcLines.filter(line => line.startsWith('K:')).map(line => line.replace(/^K:\s*/, ''));
+  return {id: tune.id, title: tune.title, keys: keys.join(', ')};
+});
+
 $(() => {
-  let abcDiv = $('.abc');
-  let abcText = abcDiv.text();
-  let tunebook = new ABCJS.TuneBook(abcText);
+  const abcDiv = $('.abc');
+  const abcText = abcDiv.text();
+  const tunebook = new ABCJS.TuneBook(abcText);
   abcDiv.hide();
-  tunebook.tunes.forEach(tune => {
-    let divId = `tune_${tune.id}`;
-    let tuneDiv = $(`<div id='${divId}'></div>`);
+  const {tunes} = tunebook;
+  const contents = $('<ul class="contents"></ul>');
+  metadata(tunes).forEach(record => {
+    const {id, title, keys} = record;
+    const listItem = $('<li></li>').text(`${id}. ${title} (${keys})`);
+    const link = $(`<a href='#${domId(record)}'></a>`).html(listItem);
+    contents.append(link);
+  });
+  contents.insertBefore(abcDiv);
+
+  tunes.forEach(tune => {
+    const divId = domId(tune);
+    const tuneDiv = $(`<div id='${divId}'></div>`);
     tuneDiv.appendTo('body');
-    let rendered = ABCJS.renderAbc(divId, tune.abc);
+    const rendered = ABCJS.renderAbc(divId, tune.abc);
   });
 });
