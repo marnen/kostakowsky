d60aad3000dccf836d1f21e6c9ebe552a2147aa5
Marnen Laibow-Koser <marnen@marnen.org>
2018-04-04 14:19:29 -0400

    Render each tune in its own div.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,15 @@
-// This is where it all goes :)
+'use strict';
+import ABCJS from 'abcjs';
+import $ from 'jquery';
+$(() => {
+  let abcDiv = $('.abc');
+  let abcText = abcDiv.text();
+  let tunebook = new ABCJS.TuneBook(abcText);
+  abcDiv.hide();
+  tunebook.tunes.forEach(tune => {
+    let divId = `tune_${tune.id}`;
+    let tuneDiv = $(`<div id='${divId}'></div>`);
+    tuneDiv.appendTo('body');
+    let rendered = ABCJS.renderAbc(divId, tune.abc);
+  });
+});
